f50b3809e8d94e4e3a33770a2b07b8f68c8c9338
T. Parmentier <66419209+TParm@users.noreply.github.com>
2021-10-18 23:56:21 +0200

    CGI without adding json

---
+++ Opdracht2-CGI/build/CMakeFiles/JSONEXAMPLE-CGI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JSONEXAMPLE-CGI.dir/jsonExample-cgi.c.o"
+  "JSONEXAMPLE-CGI"
+  "JSONEXAMPLE-CGI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/JSONEXAMPLE-CGI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
